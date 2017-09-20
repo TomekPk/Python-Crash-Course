c8d95c20f452493183efed15e853e36ad6e799da
TomekPk <src@vp.pl>
2017-09-20 21:31:00 +0200

    chapter 5

---
+++ Chapter 5/5.7.Favorite Fruit/favourite_fruit.py
@@ -0,0 +1,37 @@
+'''
+5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of
+independent if statements that check for certain fruits in your list.
+• Make a list of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit
+is in your list. If the fruit is in your list, the if block should print a statement,
+such as You really like bananas!
+'''
+
+
+fav_fruit = ["strawbery","banana","apple","pear"]
+
+print("\n1)• Make a list of your three favorite fruits and call it favorite_fruits.")
+if "strawbery" in fav_fruit:
+    print("strawbery is in the list")
+
+if "banana" in fav_fruit:
+    print("banana is in the list")
+
+if "apple" in fav_fruit:
+    print("apple is in the list")
+
+if "plump" in fav_fruit:
+    print("plump is in the list")
+else:
+    print("plump is not in the list")
+
+if "raspberry" in fav_fruit:
+    print("raspberry is in the list")
+else:
+    print("rapberry is not in the list")
+
+
+
+
+
+
